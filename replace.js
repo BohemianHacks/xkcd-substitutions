25b2aca2f2af96637f52fe942574a70b27171bce
BohemianHacks <jeff.blecha@gmail.com>
2013-11-09 20:19:31 -0600

    Update replace.js

--- replace.js
+++ replace.js
@@ -1,1 +1,27 @@
-document.body.innerHTML = document.body.innerHTML.replace(new RegExp("user1", "g"), "nobody");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("witnesses", "gi"), "These Dudes I Know");
+
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("allegedly", "gi"), "kinda probably");
+
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("new study", "gi"), "Tumblr post");
+
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("rebuild", "gi"), "Avenge");
+
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("space", "gi"), "Spaaace");
+
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("google glass", "gi"), "Virtual Boy");
+
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("smartphone", "gi"), "Pokedex");
+
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("electric", "gi"), "Atomic");
+
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("Senator", "gi"), "Elf-Lord");
+
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("car", "gi"), "Cat");
+
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("election", "gi"), "Eating Contest");
+
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("congressional leaders", "gi"), "River Spirits");
+
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("homeland security", "gi"), "Homestar Runner");
+
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("could not be reached for comment", "gi"), "is Guilty and Everyone Knows it");
